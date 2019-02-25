b5297759aa8629ede3aeea513d41c35218167929
Ben Gamari <ben@smart-cactus.org>
2019-02-25 09:49:06 -0500

    Use in-tree libfx2

--- fx2.nix
+++ fx2.nix
@@ -1,13 +1,15 @@
-{ buildPythonPackage, fetchPypi, libusb1 }:
+{ buildPythonPackage, fetchPypi, sdcc, crcmod, libusb1 }:
 
 buildPythonPackage rec {
   pname = "fx2";
   version = "0.6";
-  src = fetchPypi {
-    inherit pname version;
-    sha256 = "1n86zgld41yawas512dc141fh2fnb04y9h2acsfkj7ppzzcnq4n2";
-  };
-  buildInputs = [ ];
-  propagatedBuildInputs = [ libusb1 ];
+  src = ../vendor/libfx2;
+  #src = fetchPypi {
+  #  inherit pname version;
+  #  sha256 = "1n86zgld41yawas512dc141fh2fnb04y9h2acsfkj7ppzzcnq4n2";
+  #};
+  nativeBuildInputs = [ sdcc ];
+  propagatedBuildInputs = [ libusb1 crcmod ];
+  preConfigure = "cd software";
 }
 
