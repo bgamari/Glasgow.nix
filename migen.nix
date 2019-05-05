90497091a28a8f6a1d3b6eddb028892f74eb01a8
Ben Gamari <ben@smart-cactus.org>
2019-05-05 12:27:36 -0400

    Update migen

--- migen.nix
+++ migen.nix
@@ -3,11 +3,9 @@
 buildPythonPackage rec {
   name = "migen";
   version = "0.1";
-  src = fetchFromGitHub {
-    owner = "m-labs";
-    repo = "migen";
-    rev = "ae421054f1e3621621bcc397df171caf3681763c";
-    sha256 = "1ljj5wq5lgc1qgfxgbknshb5mwlqw6wd6va06z4gcdgbn028v7y4";
+  src = fetchGit {
+    url = "https://github.com/m-labs/migen";
+    rev = "e43cd74e7389a0a491b7aeb1f6e56b42805a25ad";
   };
   propagatedBuildInputs = [ sphinx sphinx-rtd-theme colorama ];
 }
